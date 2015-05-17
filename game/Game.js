695d398242041d31668bb7ae49f05f2d2970e575
Salim Alami <azerty_161@hotmail.com>
2015-05-17 18:02:32 +0100

    Documentation of Game class

--- game/Game.js
+++ game/Game.js
@@ -2,16 +2,28 @@ import {EventEmitter} from 'events'
 import Board from './Board'
 import cuid from 'cuid'
 
-
+/**
+ * Game Class that handle the game state
+ * @extend EventEmitter
+ */
 export default class Game extends EventEmitter {
 
+	/**
+	 * Constructor
+	 * @param  {Board} board Accepts an already existing Board instance
+	 * @param  {String} turn  Accepts an already existing turn
+	 */
 	constructor(board, turn) {
 		super()
 		// EventEmitter.call(this);
 		this.players = []
 		this.configure(board, turn)
 	}
-
+	/**
+	 * Setup of game configuration
+	 * @param  {Board} board Already existing board
+	 * @param  {String} turn  Already existing turn
+	 */
 	configure(board, turn) {
 		this.id = cuid()
 		this.turn = turn || "X"
@@ -25,7 +37,10 @@ export default class Game extends EventEmitter {
 		this.configure()
 		return this
 	}
-
+	/**
+	 * Detects the end of a game
+	 * @return {Boolean} Returns true if the game is over and false if the game is not over.
+	 */
 	detectEndOfGame() {
 		let result = this.isEndOfGame()
 		if(result) {
@@ -40,7 +55,10 @@ export default class Game extends EventEmitter {
 		}
 		return false
 	}
-
+	/**
+	 * Gives the winner of a game
+	 * @return {String|Boolean} Return the piece type of the winner or draw. If there is no winner then it returns false.
+	 */
 	isEndOfGame() {
 		let winner = this.detectWinner(); // If false then no winner
 		if(winner)
@@ -55,15 +73,22 @@ export default class Game extends EventEmitter {
 		return this.board.findWinner()
 	}
 
-
+	/**
+	 * Add a player to this game
+	 * @param {Boolean} player Returns false if the game is full or true if the player has been added to the game.
+	 */
 	addPlayer(player) {
 		if(this.players.length == 2) {
 			console.log('Game full');
 			return false;
 		}
 		this.players.push(player)
+		return true
 	}
-	
+	/**
+	 * Starts the game
+	 * @return {Boolean} Returns true or false if the game started succesfully or not. 
+	 */
 	start() {
 		if(this.players.length < 2) {
 			return false;
@@ -78,12 +103,23 @@ export default class Game extends EventEmitter {
 		this.player.X.on('make move', (x, y) => this.onPlayerPlayed(x, y, "X"))
 		this.player.O.on('make move', (x, y) => this.onPlayerPlayed(x, y, "O"))
 		this.requestAMove();
+		return true
 	}
 
+	/**
+	 * Request a move from a player that plays on the current turn of the game
+	 */
 	requestAMove() {
 		this.player[this.turn].emit('play', this.board, this.turn);
 	}
 
+	/**
+	 * This function executes when a player made a move.
+	 * @param  {Number} x    Position X in the board
+	 * @param  {Number} y    Poisition Y in the board
+	 * @param  {String} turn Current turn
+	 * @return {Boolean|undefined}  Returns false if the player did not play in the current turn.
+	 */
 	onPlayerPlayed(x, y, turn) {
 		if(this.turn != turn) {
 			console.log('Not in your turn')
@@ -95,6 +131,10 @@ export default class Game extends EventEmitter {
 		this.nextTurn();
 	}
 	
+	/**
+	 * Moves to the next turn
+	 * @return {Boolean} Returns false if the game has ended, so that the game doesn't go to the next turn.
+	 */
 	nextTurn() {
 		// Stop the loop if the game is over
 		if(this.detectEndOfGame())
