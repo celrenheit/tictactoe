d38d2a3de6c3e8eaa2888e669db99fa5747652e0
Salim Alami <azerty_161@hotmail.com>
2015-05-17 18:02:16 +0100

    Documentation of Cell class

--- game/Cell.js
+++ game/Cell.js
@@ -1,3 +1,6 @@
+/**
+ * Class Cell that describes a singe Cell in the board
+ */
 export default class Cell {
 	constructor(x, y) {
 		this.x = x;
@@ -5,6 +8,13 @@ export default class Cell {
 		this.piece = undefined;
 	}
 
+	/**
+	 * Modify the piece type of this Cell
+	 * 
+	 * @param {String} piece The piece type
+	 * @param {Boolean} force To force overwriting if an existing piece type is associated with this cell
+	 * @return {Boolean} Returns true or false if the change was successfull or failed respectively
+	 */
 	setPiece(piece, force) {
 		if(this.piece && !force) {
 			console.log('There is already a piece here')
@@ -13,11 +23,19 @@ export default class Cell {
 		this.piece = piece;
 		return true;
 	}
-
+	/**
+	 * Return the piece type
+	 * 
+	 * @return {String} The piece
+	 */
 	getPiece() {
 		return this.piece;
 	}
-
+	/**
+	 * Checks if this cell contains a piece
+	 *
+	 * @return {Boolean} Returns true or false if this cell doesn't yield a piece
+	 */
 	isEmpty() {
 		return !this.piece;
 	}
